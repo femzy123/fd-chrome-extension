a2286645754ae60dcea890b51a049f296afd9234
femzy123 <femzy123@yahoo.com>
2023-05-24 02:37:24 +0100

    Create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div className="text-3xl">
-      Hello!
-    </div>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route index path="/" element={<Home />} />
+    </Routes>
   );
 }
 
