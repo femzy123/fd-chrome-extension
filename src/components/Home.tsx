50767927d7107301f7d5c1c5119c7fd080ee718c
femzy123 <femzy123@yahoo.com>
2023-05-24 03:54:31 +0100

    Fetch summaries

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,32 +9,60 @@ interface Summary {
 }
 
 const Home = () => {
+  const [summaries, setSummaries] = useState<Summary[]>([])
+
+  const fetchSummary = () => {
+    fetch("http://localhost:5000/summaries")
+      .then((response) => response.json())
+      .then((data) => {
+        setSummaries(data)
+        console.log(data);
+      })
+      .catch((error) => {
+        // Handle any errors that occur during the request
+        console.error(error);
+      });
+  }
+
+  useEffect(() => {
+    fetchSummary();
+  }, [])
+  
   return (
-    <div className="w-full p-4 bg-zinc-200 rounded">
-      <div className="text-right">Hi, User</div>
-      <div className="flex items-center justify-start gap-4">
-        <p>Category</p>
-        <select className="rounded-lg px-2">
-          <option>All</option>
-          <option>Category 1</option>
-        </select>
-      </div>
+    <div className="w-full">
+      <div className='m-1 bg-zinc-200 p-4 rounded'>
+        <div className="text-right">Hi, User</div>
+        <div className="flex items-center justify-start gap-4">
+          <p>Category</p>
+          <select className="rounded-lg px-2">
+            <option>All</option>
+            <option>Category 1</option>
+          </select>
+        </div>
 
-      <h3 className="mt-8 text-xl font-semibold">Summaries</h3>
+        <h3 className="mt-8 text-xl font-semibold">Summaries</h3>
 
-      <div className="mt-4">
-        <Card>
-          <div className="space-y-4">
-            <Article
-              title="Highlighted Text"
-              text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam cupiditate quod totam natus nesciunt architecto nemo molestiae unde obcaecati nobis odio, aspernatur consectetur neque saepe quisquam asperiores earum deserunt atque."
-            />
-            <Article
-              title="Summary"
-              text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam cupiditate quod totam natus nesciunt architecto nemo molestiae unde obcaecati nobis odio, aspernatur consectetur neque saepe quisquam asperiores earum deserunt atque. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam cupiditate quod totam natus nesciunt architecto nemo molestiae unde obcaecati nobis odio, aspernatur consectetur neque saepe quisquam asperiores earum deserunt atque."
-            />
-          </div>
-        </Card>
+        <div className="h-[500px] mt-4 space-y-4 overflow-y-auto">
+          {summaries.length > 0 ? (
+            summaries.map((summary) => (
+              <Card>
+                <div className="space-y-4">
+                  <Article
+                    title="Highlighted Text"
+                    text={summary.highlightedText}
+                  />
+                  <Article title="Summary" text={summary.summary} />
+                </div>
+              </Card>
+            ))
+          ) : (
+            <Card>
+              <div className="h-[400px] flex items-center justify center">
+                <p className="text-gray-400 text-sm">No Summaries added yet!</p>
+              </div>
+            </Card>
+          )}
+        </div>
       </div>
     </div>
   );
